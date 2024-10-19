1e4f924b26eec39f38fd7b2a6f1ad59c82edb2fd
unknown <tagaanjustinemark3@gmail.com>
2024-10-19 11:56:34 +0800

    feat: add file upload

--- resources/js/components/posts/add.vue
+++ resources/js/components/posts/add.vue
@@ -1,6 +1,5 @@
 <template>
     <form @submit.prevent="createPost" class="max-w-sm mx-auto">
-
         <div v-if="successMessage" class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
             <span class="font-medium">{{ successMessage }}</span>
         </div>
@@ -11,10 +10,17 @@
                 <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Back</button>
             </router-link>
         </div>
+
         <div class="mb-5">
-            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="user_avatar">Upload file</label>
-            <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="user_avatar_help" id="user_avatar" type="file">
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file">Upload file</label>
+            <input 
+                type="file"
+                id="file"
+                @change="handleFileUpload"
+                class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+            />
         </div>
+
         <div class="mb-5">
             <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Post Title</label>
             <input 
@@ -26,6 +32,7 @@
                 required 
             />
         </div>
+
         <div class="mb-5">
             <label for="content" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Post Content</label>
             <textarea 
@@ -37,6 +44,7 @@
                 required
             ></textarea>
         </div>
+
         <button 
             type="submit" 
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -54,28 +62,42 @@ export default {
     name: 'posts',
     data() {
         return {
-            posts: [],
             model: {
                 post: {
                     title: '',
                     content: '',
-                    file: ''
+                    file: null
                 }
             },
             successMessage: ''
         };
     },
     methods: {   
+        handleFileUpload(event) {
+            this.model.post.file = event.target.files[0]; 
+        },
+
         async createPost() {
             try {
-                const response = await axios.post(url, this.model.post);   
-                this.posts.push(response.data); 
-                this.model.post = { title: '', content: '' };
-                this.successMessage = response.data.message
+                let formData = new FormData();
+                formData.append('title', this.model.post.title);
+                formData.append('content', this.model.post.content);
+                if (this.model.post.file) {
+                    formData.append('file', this.model.post.file); 
+                }
+
+                const response = await axios.post(url, formData, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                });
+
+                this.successMessage = response.data.message;
+                this.model.post = { title: '', content: '', file: null }; 
 
                 setTimeout(() => {
                     this.successMessage = '';
-                    window.location.href = "/posts";
+                    window.location.href = "/posts"; 
                 }, 3000);
                 
             } catch (error) {
